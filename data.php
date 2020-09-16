573583590d75b5e673b62ddd809479d27af572b0
bandhot <70566438+bandhot@users.noreply.github.com>
2020-09-15 19:50:05 -0700

    php

---
+++ data.php
@@ -0,0 +1,36 @@
+<?php
+$data_siswa=[
+        "reyhan"=>["muhammad reyhan rizki","bondowoso","24-05-2001",true],
+        "bayu"=>["raditya fajar bayu p","kendal","19-02-2003",false],
+        "toba"=>["toba fathir auladya","kendal","04-04-2005",false],
+        "yono"=>["sudiyono","kendal","10-05-2004",false],
+        "delas"=>["aodellas magosiang","semarang","14-03-2004",false],
+        "reza"=>["raza andika pratama","sukorjo","08-03-2004",false],
+        "masna"=>["matsna hidayatur rochman","magelang","01-04-2004",false],
+        "munif"=>["munif soleh","magelang","18-01-2004",false],
+        "rendi"=>["rendi saputra","magelang","01-04-2004",false],
+        "yosafat"=>["yosafat restu adi","jepara","06-09-2003",false],
+        "miftah"=>["miftachul rohman","magelang","08-10-2003",false],
+        "yuda"=>["kusuma yuda mubarok","sukorjo","25-07-2004",false],
+        "akbar"=>["muhammad akbar kurnia","sukorjo","07-03=3004",false],
+        "huda"=>["ahmad khaniful huda","magelang","21-04-2002",false],
+        "ferdi"=>["muhammad ferdiansyah","jepara","14-07-2003",false],
+        "zanu"=>["muhammad zainudin b","jepara","30-09-2003",false],
+        "reza"=>["raza aditya pratama","sukorjo","04-04-2004",false],
+        "rimba"=>["muhammad rimba abdilah","jepara","02-07-2004",false],
+        "anang"=>["anang syah amirul haqim","jepara","20-10-2003",false],
+        "reza"=>["reza fatikhul hidayat","sukorjo","06-08-2004",false],
+        "khoirudin"=>["khoirudin","sukorjo","05-09-2003",false]      
+    ];
+    // foreach($data_siswa as $key=>$value)
+    // {
+    //     if($value[3])
+    //     {
+    //         echo "$value[0]\t sudah lulus \n";
+    //     }
+    //     else{
+    //         echo "$value[0]\t belum lulus\n";
+    //     }
+    // }
+    // ?>
+    
